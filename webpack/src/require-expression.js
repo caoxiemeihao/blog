574814ea9f5cc0779fabb0019901283c73e2c0b5
草鞋没号 <308487730@qq.com>
2022-09-12 20:17:11 +0800

    learn-webpack: update require cases

--- webpack/src/require-expression.js
+++ webpack/src/require-expression.js
@@ -1,5 +1,14 @@
 function load(path) {
-  const module = require('./views/' + path)
+  const module = require('./views/' + path) //          | /^\.\/.*$/
+  // require('./views' + path)                          | /^\.\/views.*$/
+  // require('./views/' + path + '/index.js')           | /^\.\/.*\/index\.js$/
+  // require('./views/f' + path + '.js')                | /^\.\/f.*\.js$/
+  // require('./views/f' + path + '.js' + arguments[1]) | /^\.\/f.*$/
+
+  // require(`./views/${path}`)                         | /^\.\/.*$/
+  // require(`./views/${path}.js`)                      | /^\.\/.*\.js$/
+  // require(`./views/${path}.js${arguments[1]}`)       | /^\.\/.*\.js.*$/
+  // require(`./views/${path}/foo/${arguments[1]}`)     | /^\.\/.*\/foo\/.*$/
   return module
 }
 
